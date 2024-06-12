3273d910de84ea61ed38287351809d1bc71ba239
Vlad Syzov <hizes5050@gmail.com>
2024-06-12 23:15:51 +0200

    first commit

---
+++ ApiRequestMarket/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ApiRequestMarket.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
