f7dd55208f5127d57acaf9b392a819860985076b
cosmoqwe <cosmostar869@gmail.com>
2024-06-13 04:06:14 +0300

    model add

---
+++ ApiRequestMarket/Models/AddViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ApiRequestMarket;
+namespace ApiRequestMarket.Models;
+
+public class AddViewModel
+{
+    public string name { get; set; }
+    public decimal price { get; set; }
+    public int count { get; set; }
+    public string path { get; set; }
+    public string description { get; set; }
+    public Dictionary<long, string> categories = new Dictionary<long, string>();
+}
